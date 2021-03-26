73d0d8a7cc517667f936fbf25dfc1464421acad1
sreerajm104 <55447211+sreerajm104@users.noreply.github.com>
2021-03-26 08:03:19 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 mkdir -p ~/.streamlit/
-
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[general]\n\
+email = \"code2deploy@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
